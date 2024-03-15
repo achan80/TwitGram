435da441bf11e9e62135acce593173f06013b641
achan80 <chan.albert@gmail.com>
2024-03-15 01:02:36 +0000

    setup-1

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,25 @@ import { Routes, Route } from 'react-router-dom';
 import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
 import { Home } from './_root/pages';
+import RootLayout from './_root/RootLayout';
 
 
 const App = () => {
   return (
     <main className='flex h-screen'> {/* We can use semantic tags */}
       <Routes>
-        {/* Public Routes - routes that everyone will be able to see - sign up and sign in */} 
-          <Route path='/sign-in' element={<SigninForm />} />
-          <Route path='/sign-up' element={<SignupForm />} />
-        {/* Private Routes - routes you can only see when you sign in */}
-          <Route index element={<Home />}/>
+          <Route element={<AuthLayout />}>
+            {/* Public Routes - routes that everyone will be able to see - sign up and sign in */} 
+            <Route path='/sign-in' element={<SigninForm />} />
+            <Route path='/sign-up' element={<SignupForm />} />
+          </Route>
+          
+          <Route element={<RootLayout />}>
+            {/* Private Routes - routes you can only see when you sign in */}
+            <Route index element={<Home />}/>
+          </Route>
       </Routes>
       
     </main>
